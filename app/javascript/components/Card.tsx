2925bbc4a1ab22d0c707237a1c618359b3bdef62
Michael Tang <michael.tang@optimalworkshop.com>
2022-05-07 03:13:54 +1200

    Created initial Card.tsx class

---
+++ app/javascript/components/Card.tsx
@@ -0,0 +1,12 @@
+import React, {useState} from 'react'
+
+export default function Card() {
+    const [input, setInput] = useState();
+    const [output, setOutput] = useState();
+
+    return (
+        <div>
+            
+        </div>
+    )
+}
